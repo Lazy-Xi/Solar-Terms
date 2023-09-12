962b6a2a83df3c792eb7a0c0400307b7f5644e43
Laxy-Xi <lazy_xi@6fjklk.onmicrosoft.com>
2023-09-13 00:00:05 +0800

    changed the implementation of dock animation from css to js

--- scripts/dock.js
+++ scripts/dock.js
@@ -1,9 +1,12 @@
 var icons = document.getElementsByClassName("icon");
+var p;
 
 for (var i = 0; i < 24; i++) {
-    icons[i].addEventListener("mouseenter", function() {
+    icons[i].addEventListener("mouseenter", function () {
         var pre = this.previousElementSibling;
         var next = this.nextElementSibling;
+
+        this.style["transform"] = "scale(1.15) translateY(-12px)";
 
         function update(e) {
             if (e)
@@ -14,9 +17,13 @@ for (var i = 0; i < 24; i++) {
         update(next);
     });
 
-    icons[i].addEventListener("mouseleave", function() {
+    icons[i].addEventListener("mouseleave", function () {
         var pre = this.previousElementSibling;
         var next = this.nextElementSibling;
+
+        this.style["transform"] = "";
+        this.style["background-color"] = "white";
+        this.style["box-shadow"] = "0 0.125em 0.125em #0d1112";
 
         function update(e) {
             if (e)
@@ -26,4 +33,18 @@ for (var i = 0; i < 24; i++) {
         update(pre);
         update(next);
     });
+
+    icons[i].addEventListener("mousedown", function () {
+        p = this;
+        p.style["background-color"] = "rgb(200, 200, 200)";
+        p.style["box-shadow"] = "inset 1px 1px 5px white";
+
+        window.setTimeout(function () {
+        }, 100);
+    });
+
+    icons[i].addEventListener("mouseup", function() {
+        this.style["background-color"] = "white";
+        this.style["box-shadow"] = "0 0.125em 0.125em #0d1112";
+    })
 }
